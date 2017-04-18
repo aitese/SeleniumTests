41d2579b478c839d903a32d746ef4ecd5dd15482
aitese.odigie <aitese.odigie@UKD9037VY9.uk.bluee.net>
2017-04-18 17:38:19 +0100

    Concurrent windows

--- SeleniumTests/src/StaSearch.java
+++ SeleniumTests/src/StaSearch.java
@@ -2,7 +2,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,31 +10,36 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class StaSearch {
 
 	public static void main(String[] args) throws InterruptedException {
-		//System.setProperty("webdriver.chrome.driver","C:/Selenium/drivers/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:/Selenium/drivers/chromedriver.exe");
 		System.setProperty("webdriver.gecko.driver","C:/Selenium/drivers/geckodriver.exe");
 		
-		//WebDriver driver = new ChromeDriver();
-		WebDriver driver = new FirefoxDriver();
-		ImplicitWait(driver);
-		driver.get("http://ukstaging.staexperiences.com/");
+		WebDriver driverCH = new ChromeDriver();
+		WebDriver driverFF = new FirefoxDriver();
+		ImplicitWait(driverCH);
+		ImplicitWait(driverFF);
+		driverCH.get("http://ukstaging.staexperiences.com/");
+		driverFF.get("http://ukstaging.staexperiences.com/");
 		//WebElement searchBox;
 		WebElement link;
 		
 		//searchBox = driver.findElement(By.id("txtSearch"));
 		//searchBox.sendKeys("asi");
 		
-		driver.findElement(By.className("btn-primary")).click();
-		
-		ExplicitTextWait(driver, "Uncover India–Delhi to Goa");
-		
-		link = driver.findElement(By.linkText("Uncover India–Delhi to Goa"));
+		driverCH.findElement(By.className("btn-primary")).click();
+		driverFF.findElement(By.className("btn-primary")).click();
+		ExplicitTextWait(driverCH, "Uncover India–Delhi to Goa");
+		ExplicitTextWait(driverFF, "Uncover India–Delhi to Goa");
+		link = driverCH.findElement(By.linkText("Uncover India–Delhi to Goa"));
+		link = driverFF.findElement(By.linkText("Uncover India–Delhi to Goa"));
 		link.click();
 		
-		ExplicitLinkWait(driver, "btn-sta btn-primary btn-book");
+		ExplicitLinkWait(driverCH, "btn-sta btn-primary btn-book");
+		ExplicitLinkWait(driverFF, "btn-sta btn-primary btn-book");
 		
 		//searchBox.submit();
 		
-		driver.quit();
+		driverCH.quit();
+		driverFF.quit();
 	}
 	
 	public static void ImplicitWait(WebDriver driver) {
